07244c304c050147d8c052ad1275a8c290a19d80
meese-ha <81336489+meese-ha@users.noreply.github.com>
2024-11-06 11:39:10 +0900

    [Bronze V] Title: 사칙연산, Time: 164 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        
+        int A = in.nextInt();
+        int B = in.nextInt();
+        
+        in.close();
+        
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+        
+        
+        
+    }
+}
