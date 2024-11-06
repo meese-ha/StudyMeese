fe248d20dfc4dfb2d10af5d1236e7faf886da311
meese-ha <81336489+meese-ha@users.noreply.github.com>
2024-11-06 11:39:38 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        int x = in.nextInt();
+        
+        in.close();
+        
+        System.out.print(x-543);
+        
+        
+    }
+}
