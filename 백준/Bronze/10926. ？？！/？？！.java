e4b78a0f5aaabcefd3e7387caf090586f3a4c460
meese-ha <81336489+meese-ha@users.noreply.github.com>
2024-11-06 11:39:27 +0900

    [Bronze V] Title: ??!, Time: 164 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+         
+        Scanner in = new Scanner(System.in);
+        
+        String A = in.next();
+            
+         System.out.print(A+"??!");
+        
+    }    
+}
