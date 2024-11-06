280637f5f85a2bde1f0fa5f3b7da9edceab3e8a6
meese-ha <81336489+meese-ha@users.noreply.github.com>
2024-11-06 11:40:11 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        Long A = in.nextLong();
+        Long B = in.nextLong();
+        Long C = in.nextLong();
+        
+        System.out.println(A+B+C);
+        
+    }
+}
