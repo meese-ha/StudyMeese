f72efdb55e24bab73db3cccdb44875787f591848
meese-ha <81336489+meese-ha@users.noreply.github.com>
2024-11-07 12:20:37 +0900

    [Bronze V] Title: 합, Time: 168 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+       Scanner in = new Scanner(System.in);
+       
+        int A = in.nextInt();
+        in.close();
+        int sum = 0;
+        
+        for(int i=1; i<=A; i++){
+            sum += i;
+        }
+        
+        System.out.println(sum);
+        
+        
+        
+    }    
+}
