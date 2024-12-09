eaad2fd5ab5618c15ebbeaa734997b30a18c18aa
meese-ha <81336489+meese-ha@users.noreply.github.com>
2024-12-09 17:14:52 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 168 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        int[] arr = new int[31];
+        
+        for(int i=1; i<29; i++){
+            int num = in.nextInt();
+            arr[num] = 1;
+        }
+        
+        for(int i=1; i<arr.length; i++){
+              if(arr[i] != 1)
+              {
+                  System.out.println(i);    
+              }  
+        }
+        
+        in.close();
+    }
+}
