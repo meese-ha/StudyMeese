97c24ab133d6ca0d26681574c683adb3b7d15c28
meese-ha <81336489+meese-ha@users.noreply.github.com>
2025-08-21 08:31:37 +0900

    [level 1] Title: 유연근무제, Time: 0.31 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.cs
@@ -0,0 +1,49 @@
+using System;
+
+public class Solution {
+    public int solution(int[] schedules, int[,] timelogs, int startday) {
+        int answer = 0;
+        int person = 0;
+        
+        for(int i=0; i<timelogs.GetLength(0); i++)
+        {
+           int time = schedules[i]+10;
+            
+            if(time%100 >= 60){
+                time += 100;
+                time -= 60;
+            }
+            
+            person = 0;
+            
+            for(int j=0; j<7; j++)
+            {                
+            
+                if(startday >= 6)
+                {
+                    startday = startday == 6 ? 7 : 1; 
+                    continue;
+                    
+                } 
+                else 
+                {
+                     if(time >= timelogs[i,j] )
+                     {
+                         person++;
+                     } 
+                                     
+                }
+                
+                startday++;        
+            }            
+            
+            if(person == 5)
+            {
+                answer++;
+            }          
+
+        }
+        
+        return answer;
+    }
+}
